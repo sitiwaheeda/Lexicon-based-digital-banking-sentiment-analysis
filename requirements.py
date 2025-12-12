e31e2bf02fec24c9461bb26332747a11c46f678e
Jiha <89684509+sitiwaheeda@users.noreply.github.com>
2025-12-12 15:55:49 +0700

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,9 @@
+!pip install google-play-scraper
+from google_play_scraper import Sort, reviews
+from google_play_scraper import app
+
+!pip install openpyxl
+import nltk
+nltk.download('punkt')
+
+!pip install wordcloud==1.9.4
